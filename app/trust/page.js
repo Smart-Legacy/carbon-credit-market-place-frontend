350b433eca5ac9cf9fba2201c2496d861902f875
fraolb <fraolbereket@gmail.com>
2024-07-04 19:24:55 +0300

    trust page

---
+++ app/trust/page.js
@@ -0,0 +1,265 @@
+// pages/trust.tsx
+import Image from "next/image";
+
+const Trust = () => {
+  return (
+    <div className="p-4 bg-white md:px-32">
+      <h1 className="text-3xl font-bold mb-4 text-center">
+        We Build Trust Through Transparency
+      </h1>
+
+      <section className="mb-8 block md:flex">
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-bold mb-2 w-full md:w-1/2">
+            Transparency in the Voluntary Carbon Market
+          </h2>
+          <p>
+            Green Carbon Solution is committed to bringing transparency to the
+            complex voluntary carbon market. Learn about our approach.
+          </p>
+        </div>
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/bg1.png"
+            alt="Transparency"
+            width={"330"}
+            height={"330"}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </section>
+
+      <section className="mb-8 block md:flex">
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/bg1.png"
+            alt="Transparency"
+            width={"330"}
+            height={"330"}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-bold mb-2">Emissions Reduction First</h2>
+          <p>
+            We can&apos;t “offset” our way to net zero. We need to use all the
+            tools at our disposal to tackle the climate crisis. Emissions
+            reductions should always be the first priority. Carbon credits are
+            not an excuse for inaction on the important work of absolute
+            emissions reductions.
+          </p>
+        </div>
+      </section>
+
+      <section className="mb-8 block md:flex">
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-bold mb-2">
+            A Comprehensive Climate Strategy is Key
+          </h2>
+          <p>
+            When used responsibly as part of a broader climate strategy, carbon
+            credits can help organizations make an additional immediate impact
+            while longer-term emissions reduction efforts are also underway.
+          </p>
+        </div>
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/bg1.png"
+            alt="Transparency"
+            width={"330"}
+            height={"330"}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </section>
+
+      <section className="mb-8 block md:flex">
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/bg1.png"
+            alt="Transparency"
+            width={"330"}
+            height={"330"}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-bold mb-2">
+            Not All Carbon Credits Are Created Equal
+          </h2>
+          <p>
+            There are significant concerns among experts in the voluntary carbon
+            market that many of the carbon credits available today do not
+            achieve the climate benefits they claim. While quality standards are
+            still being improved, independent third-party reviews are an
+            indispensable tool for increasing transparency around carbon credit
+            quality.
+          </p>
+        </div>
+      </section>
+
+      <section className="mb-8 block md:flex">
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-bold mb-2">
+            Our Commitment to Transparency
+          </h2>
+          <p>
+            Trust is our #1 value. We&apos;ve engaged leading third-party raters
+            to build transparency into the marketplace and make carbon credit
+            purchases more trusted. We are closely monitoring the progress being
+            made around quality standards and the marketplace will evolve as new
+            standards become available.
+          </p>
+        </div>
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/bg1.png"
+            alt="Transparency"
+            width={"330"}
+            height={"330"}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </section>
+
+      <section className="mb-8 block md:flex">
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/bg1.png"
+            alt="Transparency"
+            width={"330"}
+            height={"330"}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-bold mb-2">
+            Navigating the Voluntary Carbon Market
+          </h2>
+          <p>
+            The voluntary carbon market is complex and rapidly evolving. Today,
+            finding and purchasing carbon credits with confidence is
+            challenging, and requires time and resources that many organizations
+            can&apos;t spare.
+          </p>
+        </div>
+      </section>
+
+      <section className="mb-8 block md:flex">
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-bold mb-2">Open Marketplace</h2>
+          <p>
+            Green Carbon Solution Marketplace is open to all to explore. Anyone
+            can come to the marketplace to browse carbon credit projects, see
+            detailed information and third-party ratings for most projects, plus
+            find resources to take climate action beyond carbon credits.
+          </p>
+        </div>
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/bg1.png"
+            alt="Transparency"
+            width={"330"}
+            height={"330"}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </section>
+
+      <section className="mb-8 block md:flex">
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/bg1.png"
+            alt="Transparency"
+            width={"330"}
+            height={"330"}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-bold mb-2">
+            Carbon Credits as a Force for Positive Change
+          </h2>
+          <p>
+            If used responsibly and with integrity, carbon credits have the
+            potential to be a powerful tool to fight climate change. While
+            expectations around integrity continue to evolve, every organization
+            must hold itself to high moral standards and make choices that
+            accelerate us toward our shared global goal of net zero.
+          </p>
+        </div>
+      </section>
+
+      <section className="mb-8 block md:flex">
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-bold mb-2">Ensuring Buyer Integrity</h2>
+          <p>
+            All our carbon credit providers have their own standards for buyer
+            integrity, and buyers may go through additional provider screening
+            before their purchases can be completed. Post-purchase, buyers also
+            receive resources to help them in making accurate claims about their
+            progress towards their climate goals.
+          </p>
+        </div>
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/bg1.png"
+            alt="Transparency"
+            width={"330"}
+            height={"330"}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </section>
+
+      <section className="mb-8 block md:flex">
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/bg1.png"
+            alt="Transparency"
+            width={"330"}
+            height={"330"}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-bold mb-2">
+            The Decade of the Ecopreneur Revolution
+          </h2>
+          <p>
+            Ecopreneurs are the true leaders and drivers of climate action
+            worldwide. They keep the planet at the center of everything they do,
+            and investing in them—including by funding carbon projects through
+            the purchasing of carbon credits—is key to scaling positive climate
+            impact.
+          </p>
+        </div>
+      </section>
+
+      <section className="mb-8 block md:flex">
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-bold mb-2">
+            Connecting with Ecopreneurs
+          </h2>
+          <p>
+            Green Carbon Solution Marketplace offers a seamless e-commerce
+            experience for organizations to find and purchase carbon credits
+            from innovative carbon projects developed by ecopreneurs around the
+            world.
+          </p>
+        </div>
+        <div className="w-full md:w-1/2">
+          <Image
+            src="/bg1.png"
+            alt="Transparency"
+            width={"330"}
+            height={"330"}
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Trust;
