56fc0e9b160e5cdd6ef9ca2e9efb0a3683bb8add
fraolb <fraolbereket@gmail.com>
2024-07-10 16:32:51 +0300

    changed useEffect by generateStaticParams

--- app/project/[id]/page.js
+++ app/project/[id]/page.js
@@ -1,97 +1,86 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 import Image from "next/image";
 import PurchaseCarbon from "@/components/PurchaseCarbon";
 
+const projects = [
+  {
+    id: 1,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1664300792059-863ccfe55932?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with your image path
+    title: "Katingan Peatland Restoration and Conservation",
+    region: "Asia",
+    registry: "Verra",
+    description:
+      "60-year REDD+ project in Indonesia designed to protect 150 kha of peat swamp forest from planned deforestation due to commercial plantations and logging. The project will also restore peat health by rewetting and...",
+    price: "9.91",
+  },
+  {
+    id: 2,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1664300792059-863ccfe55932?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with your image path
+    title: "Delta Blue Carbon - 1",
+    region: "Asia",
+    registry: "Verra",
+    description:
+      "The Delta Blue Carbon Project is the largest coastal blue carbon project in the world, covering 350,000 hectares of tidal wetlands in the Indus Delta region of Sindh province in Pakistan.",
+    price: "39",
+  },
+  {
+    id: 3,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1664300792059-863ccfe55932?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with your image path
+    title: "Prairie Pothole Avoided Conversion of Grasslands and Shrublands",
+    region: "Americas",
+    registry: "American Carbon Registry",
+    description:
+      "Wind sweeps almost continuously across the Dakotas, ruffling the prairie grass and the many ponds in North Dakota’s Prairie Pothole Region. The “potholes” are thousands of shallow wetlands created as the Ice Age glaciers...",
+    price: "23.92",
+  },
+  {
+    id: 4,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1664300792059-863ccfe55932?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with your image path
+    title: "Katingan Peatland Restoration and Conservation",
+    region: "Asia",
+    registry: "Verra",
+    description:
+      "60-year REDD+ project in Indonesia designed to protect 150 kha of peat swamp forest from planned deforestation due to commercial plantations and logging. The project will also restore peat health by rewetting and...",
+    price: "9.91",
+  },
+  {
+    id: 5,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1664300792059-863ccfe55932?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with your image path
+    title: "Delta Blue Carbon - 1",
+    region: "Asia",
+    registry: "Verra",
+    description:
+      "The Delta Blue Carbon Project is the largest coastal blue carbon project in the world, covering 350,000 hectares of tidal wetlands in the Indus Delta region of Sindh province in Pakistan.",
+    price: "39",
+  },
+  {
+    id: 6,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1664300792059-863ccfe55932?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with your image path
+    title: "Prairie Pothole Avoided Conversion of Grasslands and Shrublands",
+    region: "Americas",
+    registry: "American Carbon Registry",
+    description:
+      "Wind sweeps almost continuously across the Dakotas, ruffling the prairie grass and the many ponds in North Dakota’s Prairie Pothole Region. The “potholes” are thousands of shallow wetlands created as the Ice Age glaciers...",
+    price: "23.92",
+  },
+];
+
+// This function provides the dynamic paths for static generation
+export async function generateStaticParams() {
+  return projects.map((project) => ({
+    id: project.id.toString(),
+  }));
+}
+
 const ProjectPage = ({ params }) => {
-  const router = useRouter();
-  const id = params.id;
-  const [project, setProject] = useState(null);
-
-  const projects = [
-    {
-      id: 1,
-      imageUrl:
-        "https://plus.unsplash.com/premium_photo-1664300792059-863ccfe55932?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with your image path
-      title: "Katingan Peatland Restoration and Conservation",
-      region: "Asia",
-      registry: "Verra",
-      description:
-        "60-year REDD+ project in Indonesia designed to protect 150 kha of peat swamp forest from planned deforestation due to commercial plantations and logging. The project will also restore peat health by rewetting and...",
-      price: "9.91",
-    },
-    {
-      id: 2,
-      imageUrl:
-        "https://plus.unsplash.com/premium_photo-1664300792059-863ccfe55932?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with your image path
-      title: "Delta Blue Carbon - 1",
-      region: "Asia",
-      registry: "Verra",
-      description:
-        "The Delta Blue Carbon Project is the largest coastal blue carbon project in the world, covering 350,000 hectares of tidal wetlands in the Indus Delta region of Sindh province in Pakistan.",
-      price: "39",
-    },
-    {
-      id: 3,
-      imageUrl:
-        "https://plus.unsplash.com/premium_photo-1664300792059-863ccfe55932?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with your image path
-      title: "Prairie Pothole Avoided Conversion of Grasslands and Shrublands",
-      region: "Americas",
-      registry: "American Carbon Registry",
-      description:
-        "Wind sweeps almost continuously across the Dakotas, ruffling the prairie grass and the many ponds in North Dakota’s Prairie Pothole Region. The “potholes” are thousands of shallow wetlands created as the Ice Age glaciers...",
-      price: "23.92",
-    },
-    {
-      id: 4,
-      imageUrl:
-        "https://plus.unsplash.com/premium_photo-1664300792059-863ccfe55932?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with your image path
-      title: "Katingan Peatland Restoration and Conservation",
-      region: "Asia",
-      registry: "Verra",
-      description:
-        "60-year REDD+ project in Indonesia designed to protect 150 kha of peat swamp forest from planned deforestation due to commercial plantations and logging. The project will also restore peat health by rewetting and...",
-      price: "9.91",
-    },
-    {
-      id: 5,
-      imageUrl:
-        "https://plus.unsplash.com/premium_photo-1664300792059-863ccfe55932?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with your image path
-      title: "Delta Blue Carbon - 1",
-      region: "Asia",
-      registry: "Verra",
-      description:
-        "The Delta Blue Carbon Project is the largest coastal blue carbon project in the world, covering 350,000 hectares of tidal wetlands in the Indus Delta region of Sindh province in Pakistan.",
-      price: "39",
-    },
-    {
-      id: 6,
-      imageUrl:
-        "https://plus.unsplash.com/premium_photo-1664300792059-863ccfe55932?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // Replace with your image path
-      title: "Prairie Pothole Avoided Conversion of Grasslands and Shrublands",
-      region: "Americas",
-      registry: "American Carbon Registry",
-      description:
-        "Wind sweeps almost continuously across the Dakotas, ruffling the prairie grass and the many ponds in North Dakota’s Prairie Pothole Region. The “potholes” are thousands of shallow wetlands created as the Ice Age glaciers...",
-      price: "23.92",
-    },
-  ];
-
-  useEffect(() => {
-    if (id) {
-      // Simulate fetching project data from an API or database
-      const fetchProject = async () => {
-        const response = projects.find(
-          (project) => project.id.toString() === id
-        );
-        setProject(response);
-      };
-
-      fetchProject();
-    }
-  }, [id]);
+  const project = projects.find(
+    (project) => project.id.toString() === params.id
+  );
 
   if (!project) {
     return <div>Loading...</div>;
